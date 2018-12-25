51680b4ec7e6480b7e7a5914912dbda05d4e9ae8
popstk <ther@live.cn>
2018-12-25 23:52:35 +0800

    上传完毕后自动删除

--- uploadhelper.py
+++ uploadhelper.py
@@ -8,32 +8,47 @@ from utils import get_redis_client
 
 logger = logging.getLogger(__name__)
 
+
 def upload_from_queue():
     odclient = od_session.load_session(
         onedrivesdk.OneDriveClient,
-        os.path.expanduser('~/.onedrive.json'))
+        os.path.expanduser(conf["session"]))
     token = od_session.get_access_token(odclient)
 
     redisclient = get_redis_client()
     while True:
-        logger.info("Waiting...")
-        _, path = redisclient.blpop(conf["queue"])
-        path = path.decode(encoding="utf-8")
-        logger.info("Get path %s", path)
-        if not os.path.exists(path):
-            logger.error("Path not exists: %s", path)
-            continue
         try:
-            od_uploader.upload_self(
+            result = redisclient.blpop(conf["queue"], 1)
+            if result is None:
+                continue
+            path = result[1].decode(encoding="utf-8")
+            logger.info("Get path %s", path)
+            if not os.path.exists(path):
+                logger.error("Path not exists: %s", path)
+                continue
+        except KeyboardInterrupt:
+            return
+        except Exception as e:
+            logger.error(e)
+            return
+
+        try:
+            succ = od_uploader.upload_self(
                 api_base_url=odclient.base_url,
                 token=token,
                 source_file=path,
                 dest_path="od:/upload")
-            redisclient.rpush("success", path)
+            if succ:
+                os.remove(path)
+        except KeyboardInterrupt:
+            redisclient.rpush("pending", path)
         except Exception as e:
             logger.error(e)
-            redisclient.rpush("fail", path)
-        
+        finally:
+            key = "success" if succ else "fail"
+            redisclient.rpush(key, path)
+
 
 if __name__ == "__main__":
     upload_from_queue()
+    logger.info("bye")
